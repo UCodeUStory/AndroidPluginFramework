b22ae450cdbf63a7da9eba5006ba3273577d1da3
qiyue <qiyue@wiseasy.com>
2018-02-02 12:54:49 +0800

    添加插件列表

--- pluginApkDemo/app/src/main/java/com/wangpos/pluginapkdemo/WelcomeActivity.java
+++ pluginApkDemo/app/src/main/java/com/wangpos/pluginapkdemo/WelcomeActivity.java
@@ -2,21 +2,30 @@ package com.wangpos.pluginapkdemo;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.ListView;
 
 /**
  * Created by qiyue on 2018/1/31.
  */
 
-public class WelcomeActivity extends BaseActivity {
+public class WelcomeActivity extends PluginBaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        if (savedInstanceState!=null) {
-            super.onCreate(savedInstanceState);
-        }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         Log.i("qiyue","插件onCreate被调用"+savedInstanceState);
+
+        that.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_base);
+
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        listView.setAdapter(new ListAdapter(that));
+
     }
 
 
